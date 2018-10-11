ca7cd8d0a50fd5ea9c3d78f37d11c1fad06ddbcf
oracle <oracle@bigdatalite.localdomain>
2018-10-10 20:50:20 -0400

    Solution to Problem 1

---
+++ AOCDay1p1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main( void ) {
+	string input;
+	int digit_sum;
+	
+	cin >> input;
+	digit_sum = 0;
+	for( unsigned int i = 0; i < input.size(); ++i ) {
+		int next_digit = i == (input.size() - 1) ? 0 : (i + 1);
+		if( input[i] == input[next_digit] ) {
+			digit_sum += input[next_digit] - '0';
+		}
+	}
+	cout << digit_sum << endl;
+
+	return EXIT_SUCCESS;
+}
