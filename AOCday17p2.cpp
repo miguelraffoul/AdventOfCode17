dae9b5f75653f43b0600c4dea565eff3d4fc6a6b
oracle <oracle@bigdatalite.localdomain>
2018-11-14 00:23:49 -0500

    Fixed typo in part 2

--- AOCday17p2.cpp
+++ AOCday17p2.cpp
@@ -1,16 +1,13 @@
 #include <cstdlib>
 #include <iostream>
-#include <stack>
 
 using namespace std;
 
 int main( void ) {
-    stack<int> buffer;
-    int jumps, values;
+    int buffer, jumps, values;
     int curr_pos;
 
     cin >> jumps >> values;
-    buffer.push( 0 );
     curr_pos = 0;
     for( int i = 1; i <= values; ++i ) {
         if( (curr_pos + jumps) >= i )
@@ -19,9 +16,9 @@ int main( void ) {
             curr_pos = curr_pos + jumps;
         curr_pos += 1;
         if( curr_pos == 1 )
-            buffer.push( i );
+            buffer = i;
     }
-    cout << buffer.top() << endl;
+    cout << buffer << endl;
 
     return EXIT_SUCCESS; 
 }
