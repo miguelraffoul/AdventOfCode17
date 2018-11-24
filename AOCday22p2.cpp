2effc0d13ece65a0c436e15fd0c23d709a031ec6
oracle <oracle@bigdatalite.localdomain>
2018-11-23 19:50:13 -0500

    Solutions to Problem 22

--- AOCday22p2.cpp
+++ AOCday22p2.cpp
@@ -7,15 +7,16 @@
 
 using namespace std;
 
-unordered_map<string, bool> getInfectionMap( const vector<string>& );
+unordered_map<string, char> getInfectionMap( const vector<string>& );
 string posToSring( const int&, const int& );
-int getInfectionCount( unordered_map<string, bool>&, const int& );
+int getInfectionCount( unordered_map<string, char>&, const int& );
 char getRightDir( const char& );
 char getleftDir( const char& );
+char getReverseDir( const char& );
 pair<int, int> getNextMove( const char& );
 
 int main( void ) {
-    unordered_map<string, bool> infection_map;
+    unordered_map<string, char> infection_map;
     vector<string> grid;
     string input;
 
@@ -23,17 +24,17 @@ int main( void ) {
         grid.push_back( input );
     }
     infection_map = getInfectionMap( grid );
-    cout << getInfectionCount( infection_map, 10000 ) << endl;
+    cout << getInfectionCount( infection_map, 10000000 ) << endl;
 
     return EXIT_SUCCESS; 
 }
 
-unordered_map<string, bool> getInfectionMap( const vector<string>& grid ) {
-    unordered_map<string, bool> i_map;
+unordered_map<string, char> getInfectionMap( const vector<string>& grid ) {
+    unordered_map<string, char> i_map;
     for( unsigned int row = 0; row < grid.size(); ++row ) {
         for( unsigned int col = 0; col < grid[row].size(); ++col ) {
             if( grid[row][col] == '#' )
-                i_map[posToSring( row, col )] = true;
+                i_map[posToSring( row, col )] = 'I';
         }
     }
     return i_map;
@@ -47,22 +48,34 @@ string posToSring( const int& row, const int& col ) {
     return oss.str();
 }
 
-int getInfectionCount( unordered_map<string, bool>& infection_map, const int& bursts ) {
+int getInfectionCount( unordered_map<string, char>& infection_map, const int& bursts ) {
     char dir = 'u';
     int row, col;
     int inf_count = 0;    
     row = col = 12;
     for( int b = 0; b < bursts; ++b ) {
         string pos_str = posToSring( row, col );
-        if( infection_map.find( pos_str ) != infection_map.end() &&
-            infection_map[pos_str] ) {
-            dir = getRightDir( dir );
-            infection_map[pos_str] = false;
+        if( infection_map.find( pos_str ) != infection_map.end() ) {
+            if( infection_map[pos_str] == 'C' ) {
+                dir = getleftDir( dir );
+                infection_map[pos_str] = 'W';
+            }
+            else if( infection_map[pos_str] == 'W' ) {
+                infection_map[pos_str] = 'I';
+                ++inf_count;
+            }
+            else if( infection_map[pos_str] == 'I' ) {
+                dir = getRightDir( dir );
+                infection_map[pos_str] = 'F';
+            }
+            else { //infection_map[pos_str] == 'F'
+                dir = getReverseDir( dir );
+                infection_map[pos_str] = 'C';
+            }
         }
         else {
             dir = getleftDir( dir );
-            infection_map[pos_str] = true;
-            ++inf_count;
+            infection_map[pos_str] = 'W';
         }
         pair<int, int> move = getNextMove( dir );
         row += move.first;
@@ -87,6 +100,16 @@ char getleftDir( const char& dir ) {
         case 'l': return 'd';
         case 'd': return 'r';
         case 'r': return 'u';
+    }
+    return '-';
+}
+
+char getReverseDir( const char& dir ) {
+    switch( dir ) {
+        case 'u': return 'd';
+        case 'l': return 'r';
+        case 'd': return 'u';
+        case 'r': return 'l';
     }
     return '-';
 }
